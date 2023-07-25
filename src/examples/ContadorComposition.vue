4eabac9e3e1f100040794501fcba8883d838c46d
alejox <alejoxdrums@gmail.com>
2023-07-24 20:35:49 -0500

    add basic examples

---
+++ src/examples/ContadorComposition.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+
+const count = ref(0);
+
+function increment() {
+  count.value++;
+}
+
+function decrement() {
+  count.value -= 1
+}
+
+onMounted(() => {
+  console.log(`El valor inicial de contador es ${count.value}`);
+});
+</script>
+
+<template>
+  <h1>Hola soy la versión de Composition API</h1>
+  <h1>Contador: {{ count }}</h1>
+  <button @click="decrement">Decrementar contador</button>
+  <button @click="increment">Incrementar contador</button>
+</template>
+
+<style scoped></style>
---
+++ src/examples/ContadorOptions.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    increase() {
+      this.count++;
+    },
+    decrease() {
+      this.count--;
+    },
+  },
+  mounted() {
+    console.log(`El valor inicial del contador es: ${this.count}`);
+  },
+};
+</script>
+
+<template>
+  <h1>Contador: {{ count }}</h1>
+  <button @click="decrease">Disminuir contador</button>
+  <button @click="increase">Aumentar contador</button>
+</template>
+
+<style scoped></style>
